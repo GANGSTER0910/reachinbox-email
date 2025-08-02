5c3a569f92724ea20753b15a0fa4cf73738868af
HarshPanchal0910 <harsh.p4@ahduni.edu.in>
2025-08-02 16:40:54 +0530

    update elastic_search

--- backend/src/services/elasticsearch.ts
+++ backend/src/services/elasticsearch.ts
@@ -2,4 +2,7 @@ import { Client } from '@elastic/elasticsearch';
 const ELASTIC_SEARCH_URL = process.env.ELASTIC_SEARCH_URL || 'http://localhost:9200';
 export const esClient = new Client({
   node: ELASTIC_SEARCH_URL,
+  auth:{
+    apiKey: process.env.ELASTIC_SEARCH_API_KEY || '',
+  }
 });
